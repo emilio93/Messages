b7576b383c6e4966b44c7f639c97eb1fee05ed33
Marjorie Benavides <majobm2433@gmail.com>
2021-08-04 13:45:13 -0600

    Add class Message

---
+++ Models/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messages.Models
+{
+    public class Message : IMessage
+    {
+        public int Id { get ; set ; }
+        public string Subject { get  ; set ; }
+        public string Content { get  ; set ; }
+        public string Author { get  ; set ; }
+        public DateTime SentAt { get ; set ; }
+
+        /*
+         * Get data
+         */
+        public Message(int id, string subject, string content, string author, DateTime sentAt)
+        {
+            this.Id = id;
+            this.Subject = subject;
+            this.Content = content;
+            this.Author = author;
+            this.SentAt = sentAt;
+        }
+
+        /*
+         * insert database
+         */
+        public Message(string subject, string content, string author, DateTime sentAt)
+        {
+            this.Id = 0;
+            this.Subject = subject;
+            this.Content = content;
+            this.Author = author;
+            this.SentAt = sentAt;
+        }
+    }
+}
