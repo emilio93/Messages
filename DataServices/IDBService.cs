00a8f8d5bf3cde3c300ba867afee75a9ac44d566
Emilio Rojas <emilio93@gmail.com>
2021-08-07 01:08:58 -0600

    Add DBService to connect and query the database

---
+++ DataServices/IDBService.cs
@@ -0,0 +1,15 @@
+﻿using Messages.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messages.DataServices
+{
+    public interface IDBService
+    {
+        public Task<DataTable> getAllMessages();
+        public Task<bool> StoreMessage(IMessage message);
+    }
+}
